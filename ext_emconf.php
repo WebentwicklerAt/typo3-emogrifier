050900e5d46159677b71573d7c32989c7190a670
Gernot Leitgab <gernot@leitgab.at>
2020-09-20 21:44:35 +0200

    [BUGFIX] version requirement

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'Emogrifier',
     'description' => 'Converts CSS styles into inline HTML styles.',
     'category' => 'fe',
-    'version' => '4.0.0',
+    'version' => '4.0.1',
     'state' => 'stable',
     'uploadfolder' => 0,
     'createDirs' => '',
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'Webentwickler.at',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.99-10.4.99',
+            'typo3' => '9.5.0-10.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
