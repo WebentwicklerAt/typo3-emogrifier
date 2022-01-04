d2bb48fb3a6342b2f0433d708af77f54aa923a88
Gernot Leitgab <gernot@leitgab.at>
2022-01-04 14:56:21 +0100

    [FEATURE] add base64 inline view helper

---
+++ Classes/ViewHelpers/Base64InlineViewHelper.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace WebentwicklerAt\Emogrifier\ViewHelpers;
+
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+use TYPO3Fluid\Fluid\Core\ViewHelper\Traits\CompileWithRenderStatic;
+
+class Base64InlineViewHelper extends AbstractViewHelper
+{
+    use CompileWithRenderStatic;
+
+    /**
+     * @var bool
+     */
+    protected $escapeChildren = false;
+
+    /**
+     * @var bool
+     */
+    protected $escapeOutput = false;
+
+    /**
+     * @return void
+     */
+    public function initializeArguments()
+    {
+        parent::initializeArguments();
+        $this->registerArgument('path', 'string', 'The path and filename of the resource file.', true);
+    }
+
+    /**
+     * @param array $arguments
+     * @param \Closure $renderChildrenClosure
+     * @param RenderingContextInterface $renderingContext
+     * @return string
+     */
+    public static function renderStatic(
+        array $arguments,
+        \Closure $renderChildrenClosure,
+        RenderingContextInterface $renderingContext
+    )
+    {
+        $path = GeneralUtility::getFileAbsFileName($arguments['path']);
+        $content = GeneralUtility::getUrl($path);
+        $contentType = mime_content_type($path);
+        $base64EncodedContent = base64_encode($content);
+        return 'data:' . $contentType . ';base64,' . $base64EncodedContent;
+    }
+}
