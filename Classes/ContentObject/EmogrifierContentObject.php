7ad2609b707537d5eea606abc25a06e43958267b
Gernot Leitgab <gernot@leitgab.at>
2022-09-23 12:24:58 +0200

    Add support for non-cached objects

--- Classes/ContentObject/EmogrifierContentObject.php
+++ Classes/ContentObject/EmogrifierContentObject.php
@@ -2,7 +2,9 @@
 
 namespace WebentwicklerAt\Emogrifier\ContentObject;
 
+use Psr\Http\Message\ServerRequestInterface;
 use TYPO3\CMS\Frontend\ContentObject\AbstractContentObject;
+use TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController;
 use WebentwicklerAt\Emogrifier\Utility\EmogrifierUtility;
 
 class EmogrifierContentObject extends AbstractContentObject
@@ -20,6 +22,26 @@ class EmogrifierContentObject extends AbstractContentObject
             $css = $this->cObj->cObjGetSingle($conf['css'], $conf['css.']);
         }
         $extractContent = (array_key_exists('extractContent', $conf) && $conf['extractContent']);
+
+
+        /** @var TypoScriptFrontendController $controller */
+        $controller = $GLOBALS['TSFE'];
+        /** @var ServerRequestInterface $request */
+        $request = $GLOBALS['TYPO3_REQUEST'];
+
+        // Render non-cached page parts by replacing placeholders which are taken from cache or added during page generation
+        if ($controller->isINTincScript()) {
+            if (!$controller->isGeneratePage()) {
+                // When page was generated, this was already called. Avoid calling this twice.
+                $controller->preparePageContentGeneration($request);
+            }
+            $contentBak = $controller->content;
+            $controller->content = $content;
+            $controller->INTincScript();
+            $content = $controller->content;
+            $controller->content = $contentBak;
+            unset($contentBak);
+        }
 
         $options = [];
         if (array_key_exists('options.', $conf) && is_array($conf['options.'])) {
