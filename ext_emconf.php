8adbbb1ba2cf19c1c6468d2e562a87b2286b3908
Gernot Leitgab <gernot@leitgab.at>
2023-06-29 08:53:40 +0200

    [BUGFIX] version information

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,13 +4,17 @@ $EM_CONF[$_EXTKEY] = [
     'description' => 'Converts CSS styles into inline HTML styles.',
     'category' => 'fe',
     'clearCacheOnLoad' => true,
-    'version' => '5.0.0',
+    'version' => '6.0.1',
     'state' => 'stable',
     'author' => 'Gernot Leitgab',
     'author_company' => 'Webentwickler.at',
     'constraints' => [
         'depends' => [
+<<<<<<< HEAD
             'typo3' => '10.4.0-12.4.99',
+=======
+            'typo3' => '11.5.0-11.5.99',
+>>>>>>> 8796f68 ([BUGFIX] version information)
         ],
         'conflicts' => [],
         'suggests' => [],
